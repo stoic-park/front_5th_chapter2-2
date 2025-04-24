6c903490cba6a724403ef816f26d5e0c2ea15393
stoic-park <demige79@gmail.com>
2025-04-24 21:38:07 +0900

    refactor: 초기값 상수 분리

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -1,52 +1,18 @@
 import { CartPage } from './pages/CartPage.tsx';
 import { AdminPage } from './pages/AdminPage.tsx';
-import { Coupon, Product } from '../types.ts';
 import { ProductProvider, CouponProvider, CartProvider } from './provider';
 import { useLocalStorage } from './hooks/useLocalStorage';
 import { useAuth } from './hooks/useAuth';
+import { initialProducts } from './constants/product';
+import { initialCoupons } from './constants/coupon';
 
-// FIXME: 상수로 빼자
-const initialProducts: Product[] = [
-    {
-        id: 'p1',
-        name: '상품1',
-        price: 10000,
-        stock: 20,
-        discounts: [
-            { quantity: 10, rate: 0.1 },
-            { quantity: 20, rate: 0.2 },
-        ],
-    },
-    {
-        id: 'p2',
-        name: '상품2',
-        price: 20000,
-        stock: 20,
-        discounts: [{ quantity: 10, rate: 0.15 }],
-    },
-    {
-        id: 'p3',
-        name: '상품3',
-        price: 30000,
-        stock: 20,
-        discounts: [{ quantity: 10, rate: 0.2 }],
-    },
-];
-
-const initialCoupons: Coupon[] = [
-    {
-        name: '5000원 할인 쿠폰',
-        code: 'AMOUNT5000',
-        discountType: 'amount',
-        discountValue: 5000,
-    },
-    {
-        name: '10% 할인 쿠폰',
-        code: 'PERCENT10',
-        discountType: 'percentage',
-        discountValue: 10,
-    },
-];
+// TODO:
+// 0. AdminPage 컴포넌트 분리
+// 1. 상수 분리
+// 2. 배포
+// 3. 제출
+// 4. 테스트 코드 작성
+// 5. PR 작성
 
 const App = () => {
     const { isAdmin, setIsAdmin } = useAuth();
--- src/refactoring/hooks/useLocalStorage.ts
+++ src/refactoring/hooks/useLocalStorage.ts
@@ -1,5 +1,3 @@
-// FIXME: useCart 훅처럼 사용할 수 있게, 값을 가져오고 셋 할수 있도록 수정
-// useState, useEffect 사용했을 때와 차이점은?
 export const useLocalStorage = () => {
     const getLocalStorage = (key: string) => {
         const value = localStorage.getItem(key);
