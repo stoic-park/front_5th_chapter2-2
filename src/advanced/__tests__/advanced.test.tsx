5145e42d3c7000257fdb8cfe7ab85707ce4f7241
stoic-park <demige79@gmail.com>
2025-04-24 22:34:52 +0900

    test: provider 관련 에러 수정

--- src/advanced/__tests__/advanced.test.tsx
+++ src/advanced/__tests__/advanced.test.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
 import { describe, expect, test } from 'vitest';
 import { act, fireEvent, render, screen, within } from '@testing-library/react';
 import { CartPage } from '../../refactoring/pages/CartPage';
 import { AdminPage } from '../../refactoring/pages/AdminPage';
 import { Coupon, Product } from '../../types';
+import { ProductProvider, CouponProvider, CartProvider } from '../../refactoring/provider';
 
 const mockProducts: Product[] = [
     {
@@ -44,36 +44,29 @@ const mockCoupons: Coupon[] = [
 ];
 
 const TestAdminPage = () => {
-    const [products, setProducts] = useState<Product[]>(mockProducts);
-    const [coupons, setCoupons] = useState<Coupon[]>(mockCoupons);
-
-    const handleProductUpdate = (updatedProduct: Product) => {
-        setProducts((prevProducts) => prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p)));
-    };
-
-    const handleProductAdd = (newProduct: Product) => {
-        setProducts((prevProducts) => [...prevProducts, newProduct]);
-    };
-
-    const handleCouponAdd = (newCoupon: Coupon) => {
-        setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
-    };
-
     return (
-        <AdminPage
-            products={products}
-            coupons={coupons}
-            onProductUpdate={handleProductUpdate}
-            onProductAdd={handleProductAdd}
-            onCouponAdd={handleCouponAdd}
-        />
+        <ProductProvider initialProducts={mockProducts}>
+            <CouponProvider initialCoupons={mockCoupons}>
+                <CartProvider>
+                    <AdminPage />
+                </CartProvider>
+            </CouponProvider>
+        </ProductProvider>
     );
 };
 
 describe('advanced > ', () => {
     describe('시나리오 테스트 > ', () => {
         test('장바구니 페이지 테스트 > ', async () => {
-            render(<CartPage products={mockProducts} coupons={mockCoupons} />);
+            render(
+                <ProductProvider initialProducts={mockProducts}>
+                    <CouponProvider initialCoupons={mockCoupons}>
+                        <CartProvider>
+                            <CartPage />
+                        </CartProvider>
+                    </CouponProvider>
+                </ProductProvider>
+            );
             const product1 = screen.getByTestId('product-p1');
             const product2 = screen.getByTestId('product-p2');
             const product3 = screen.getByTestId('product-p3');
